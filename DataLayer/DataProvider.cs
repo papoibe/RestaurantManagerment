375e923768fd0a07e96734190947db6f6ca4b1f1
papoibe <lenguyenphuocthinh1234@gmail.com>
2025-04-09 22:30:59 +0700

    Update DataProvider.cs tren nhanh main

--- DataLayer/DataProvider.cs
+++ DataLayer/DataProvider.cs
@@ -3,10 +3,103 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace DataLayer
 {
-    class DataProvider
+    public class DataProvider
     {
+        protected SqlConnection cn;
+
+        public DataProvider()
+        {
+            string cnStr = "Data Source=LAPTOP-960OHC3A\\THINH;Initial Catalog=QuanLyQuanAn;Integrated Security=True;Trust Server Certificate=True";
+            cn = new SqlConnection(cnStr);
+        }
+
+        public void Connect()
+        {
+            try
+            {
+                if (cn != null && cn.State == ConnectionState.Closed)
+                {
+                    cn.Open();
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void DisConnect()
+        {
+            try
+            {
+                if (cn != null && cn.State == ConnectionState.Open)
+                {
+                    cn.Close();
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+        }
+
+        public object MyExecuteScalar(string sql, CommandType type)
+        {
+            try
+            {
+                Connect();
+                SqlCommand cmd = new SqlCommand(sql, cn);
+                cmd.CommandType = type;
+                return cmd.ExecuteScalar();
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                DisConnect();
+            }
+        }
+
+        public SqlDataReader MyExecuteReader(string sql, CommandType type)
+        {
+            try
+            {
+                Connect();
+                SqlCommand cmd = new SqlCommand(sql, cn);
+                cmd.CommandType = type;
+                return cmd.ExecuteReader(CommandBehavior.CloseConnection);
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+        }
+
+        public int MyExecuteNonQuery(string sql, CommandType type)
+        {
+            try
+            {
+                Connect();
+                SqlCommand cmd = new SqlCommand(sql, cn);
+                cmd.CommandType = type;
+                return cmd.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                DisConnect();
+            }
+        }
     }
 }
